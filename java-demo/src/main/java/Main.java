57de37665511f97107cc011ef382ac86ba026fd5
dstaraster1969 <71042914+dstaraster1969@users.noreply.github.com>
2023-08-07 08:47:15 -0700

    Add a stopwatch

--- java-demo/src/main/java/Main.java
+++ java-demo/src/main/java/Main.java
@@ -2,6 +2,7 @@ import org.json.JSONArray;
 
 public class Main {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         // Parse command line args
         if(args.length != 2) {
             System.out.println("java main.java numPages, filters (a JSON object)");
@@ -18,6 +19,7 @@ public class Main {
         } catch(Exception e) {
             System.exit(1);
         }
-
+        long end = System.currentTimeMillis();
+        System.out.println("Elapsed time " + (end - start));
     }
 }
