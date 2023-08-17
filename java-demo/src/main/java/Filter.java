a81a123261a745a4640c64c790b8ad2f6a2cc53c
dstaraster1969 <71042914+dstaraster1969@users.noreply.github.com>
2023-08-17 08:19:46 -0700

    fixed bug

--- java-demo/src/main/java/Filter.java
+++ java-demo/src/main/java/Filter.java
@@ -2,11 +2,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.security.KeyException;
 import java.util.*;
 
 public class Filter {
+    // filterAll() filters all shows against all filters
     protected JSONArray filterAll(JSONArray unfiltered, String filters) throws JsonProcessingException {
         JSONArray filter_array = new JSONArray(filters);
         JSONArray results = applyFilter(unfiltered, filter_array.getJSONObject(0));
@@ -17,6 +16,7 @@ public class Filter {
         return results;
     }
 
+    // applyFilter() applies a single filter to all shows
     private JSONArray applyFilter(JSONArray objs, JSONObject filter) throws JsonProcessingException {
         JSONArray results = new JSONArray();
         for(int i = 0; i < objs.length(); i++) {
@@ -28,6 +28,7 @@ public class Filter {
         return results;
     }
 
+    // checkForMatch() decides whether there is a match between the show and the filter
     private Boolean checkForMatch(JSONObject show, JSONObject filter) throws JsonProcessingException {
         if(Objects.equals(filter, new JSONObject("{}"))) {
             return true;
@@ -36,11 +37,18 @@ public class Filter {
         HashMap<String, Object> show_map = mapper.readValue(show.toString(), HashMap.class);
         HashMap<String, Object> filter_map = mapper.readValue(filter.toString(), HashMap.class);
         String key = (String) filter_map.keySet().toArray()[0];
-        if(show_map.get(key) == null) {
-            return false;
-        } else if(show_map.get(key).getClass() != LinkedHashMap.class) {
+
+        // If we're at a leaf whose value is null, see if the null matches both the show and filter.
+        // This has to be in its own check because subsequent checks look at the class of the value
+        // which doesn't work for null
+        if(show_map.get(key) == null || filter_map.get(key) == null) {
+            return show_map.get(key) == filter_map.get(key);
+        }
+        if(show_map.get(key).getClass() != LinkedHashMap.class) {
+            // if the value for the key isn't a hashmap, then it's a leaf
             return show.get(key).equals(filter.get(key));
         } else {
+            // We aren't at a leaf, so call recursively
             return checkForMatch((JSONObject) show.get(key), (JSONObject) filter.get(key));
         }
     }
